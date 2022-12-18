b6b356a2c28b2197f28edb0a0ca6ef5c824db012
Cracked Finn <59065228+CrackedFinn@users.noreply.github.com>
2022-12-18 23:12:54 +0300

    Add files via upload
    Release 0.1

---
+++ ddmAPI.py
@@ -0,0 +1,40 @@
+from qqddm import AnimeConverter, InvalidQQDDMApiResponseException, IllegalPictureQQDDMApiResponseException
+from PIL import Image
+from io import BytesIO
+import requests
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_VERSION = 2
+USERAGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0"
+PROXY = os.getenv('TOKEN')
+
+
+def GetImage(picture_bytes, width, height):
+    if width >= height:
+        image_is_wide = True  # image is wide / square
+    else:
+        image_is_wide = False  # image is vertical
+
+    converter = AnimeConverter(generate_api_version=API_VERSION, global_useragents=[USERAGENT], global_proxy=PROXY)
+
+    try:
+        result = converter.convert(picture_bytes)
+        url = [str(url) for url in result.pictures_urls][2]
+        im = Image.open(BytesIO(requests.get(url).content))
+        if image_is_wide:
+            cropped = im.crop((19, 19, 1097, 737))  # (left, top, right, bottom)
+        else:
+            cropped = im.crop((29, 31, 797, 1184))  # (left, top, right, bottom)
+        im.close()
+        return cropped
+    except IllegalPictureQQDDMApiResponseException:
+        return "The image provided is forbidden, try with another picture"
+    except InvalidQQDDMApiResponseException as ex:
+        if json.loads(ex.response_body)["code"]==1001:
+            return "❌ It looks like there is no human face at the image. *Try uploading a photo of a person.*"
+        else:
+            return "❌ Oups... An error occurred while processing your photo. Please contact support with the following information:\n\n_" + f"API returned error ({ex}); response body: {ex.response_body}" + "_"
---
+++ main.py
@@ -0,0 +1,80 @@
+from aiogram import Bot, Dispatcher, executor, types
+from io import BytesIO
+import ddmAPI
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = Bot(token=os.getenv('TOKEN'))
+dp = Dispatcher(bot)
+
+
+def init_db():
+    return mysql.connector.connect(
+        host=os.getenv("HOST"),
+        user=os.getenv("DB_USERNAME"),
+        passwd=os.getenv("PASSWORD"),
+        database=os.getenv("DATABASE"),
+    )
+
+
+mydb = init_db()
+
+
+def get_cursor():
+    global mydb
+    try:
+        mydb.ping(reconnect=True, attempts=3, delay=5)
+    except mysql.connector.Error as err:
+        mydb = init_db()
+    return mydb.cursor()
+
+
+@dp.message_handler(content_types=['document'])
+async def fileHandle(message):
+    await message.reply("🖼️ Please send me the picture as a *'Photo'*, not as a *'File'*",
+                        parse_mode="Markdown")
+
+
+@dp.message_handler(content_types=['photo'])
+async def handle_docs_photo(message):
+    image = BytesIO()
+    msg_photo = message.photo[-1]
+    bytes = await msg_photo.download(destination_file=image)
+    converted_image_pil = ddmAPI.GetImage(bytes.read(), msg_photo["width"], msg_photo["height"])
+    if isinstance(converted_image_pil, str):  # Exception
+        await message.reply(converted_image_pil, parse_mode="Markdown")
+    else:
+        bio = BytesIO()
+        bio.name = 'image.jpeg'
+        converted_image_pil.save(bio, 'JPEG')
+        bio.seek(0)
+        await message.reply_photo(bio)
+
+
+@dp.message_handler(commands=['start'])  # Run after /start command
+async def send_welcome(message: types.Message):
+    # ADD NEW USER TO DB #
+    mycursor = get_cursor()
+    sql = "SELECT * FROM MeInComicsUsers WHERE TelegramUserID = %s"
+    val = (message.chat.id,)
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchall()
+    if len(myresult) == 0:
+        print(1)
+        sql = "INSERT INTO MeInComicsUsers (TelegramUserID) VALUES (%s)"
+        val = (message.chat.id,)
+        mycursor.execute(sql, val)
+        mydb.commit()
+    mycursor.close()
+    # ADD NEW USER TO DB #
+
+    await message.answer(
+        "👨‍🎨 Hi! Using the bot *“Me In Comics”* _(formerly known as Different Dimension Me)_ anyone can easily create their own anime versions of their photos.\n\n*Just send me images in this chat and I will transform them!*",
+        parse_mode="Markdown")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=False)
